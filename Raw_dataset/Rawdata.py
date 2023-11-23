6228eb40eec1741f42c30bc73822bde5eddd26be
Adamhuang <shinmohuang@163.com>
2023-11-23 22:05:47 +0000

    add: training.py

--- Raw_dataset/Rawdata.py
+++ Raw_dataset/Rawdata.py
@@ -2,7 +2,7 @@ from scipy.io import loadmat
 import pandas as pd
 
 # Use raw string for the path
-path = r'D:\OneDrive - University of Bristol\FurtherResearch\Raw_dataset\OneDrive_1_2023-11-5\S1\S1_Session1_EMG.mat'
+path = r'C:\OneDrive - University of Bristol\FurtherResearch\Raw_dataset\OneDrive_1_2023-11-5\S1\S1_Session1_EMG.mat'
 
 # Load the .mat file
 features_struct = loadmat(path)
